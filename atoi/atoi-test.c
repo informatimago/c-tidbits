514431fba506da942fdb7433345c3756569f641a
Pascal J. Bourguignon <pjb@informatimago.com>
2022-01-12 12:14:44 +0100

    Added strtol;
    atoi doesn't parse 0xhh 0ooo 0bBBB.
    strtol doesn't parse 0bBBB.
    (but strtol(str,&end,2) would parse BBB).

--- atoi/atoi-test.c
+++ atoi/atoi-test.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc,char** argv)
 {
     int i;
     for(i=1;i<argc;i++){
-        printf("%s -> %d\n",(argv[i]),atoi(argv[i]));
+        printf("atoi(\"%s\") -> %d\n",(argv[i]),atoi(argv[i]));
+    }
+    for(i=1;i<argc;i++){
+        char* end;
+        end=argv[i]+strlen(argv[i]);
+        printf("strtol(\"%s\",0) -> %ld\n",(argv[i]),strtol(argv[i],&end,0));
     }
     return 0;
 }
